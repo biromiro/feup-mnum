4268d5d6aa40f90f7c65cc5f2c09d9dcea74cd29
Nuno Costa <up201906272@fe.up.pt>
2020-12-02 17:09:34 +0000

    a9 new func

--- a9/main.py
+++ a9/main.py
@@ -42,7 +42,16 @@ def delta_y(x,y):
 def g(x,y):
     return y**2 - 2*x*y - 6*y + 2*x**2+12
 
-def find_local_minima_multivariable(xn,yn,gradient,h):
+def delta_x_2(x,y):
+    return math.cos(x) + 2*(x+1)
+
+def delta_y_2(x,y):
+    return 2*(y-4)
+
+def h(x,y):
+    return (x+1)**2 + (y-4)**2 + math.sin(x)
+
+def find_local_minima_multivariable(xn,yn,j, gradient,h):
     xn1 = xn + 1
     yn1 = yn + 1
     while(abs(xn1-xn) > 0.01):
@@ -50,17 +59,38 @@ def find_local_minima_multivariable(xn,yn,gradient,h):
         yn = yn1
         xn1 = xn - h*gradient[0](xn,yn)
         yn1 = yn - h*gradient[1](xn,yn)
-        if(g(xn1,yn1)<g(xn,yn)):
+        if(j(xn1,yn1)<j(xn,yn)):
             h = h*2
         else:
             h = h/2
     return (xn1,yn1)
 
+def find_local_maxima_multivariable(xn,yn,j, gradient,h):
+    xn1 = xn + 1
+    yn1 = yn + 1
+    while(abs(xn1-xn) > 0.01):
+        xn = xn1
+        yn = yn1
+        xn1 = xn - h*gradient[0](xn,yn)
+        yn1 = yn - h*gradient[1](xn,yn)
+        if(j(xn1,yn1)>j(xn,yn)):
+            h = h*2
+        else:
+            h = h/2
+    return (xn1,yn1)
+
+
 if __name__ == '__main__':
     print(find_local_minima(-1,0))
     print(find_local_maxima(-1,0))
-    x,y = find_local_minima_multivariable(1,1,[delta_x,delta_y],1)
+    x,y = find_local_minima_multivariable(1,1,g,[delta_x,delta_y],1)
     print(x,y,g(x,y))
+    x, y = find_local_minima_multivariable(0, 0, h, [delta_x_2, delta_y_2], 1)
+    print(x, y, h(x, y))
+    x, y = find_local_maxima_multivariable(1, 1, g, [delta_x, delta_y], 1)
+    print(x, y, g(x, y))
+    x, y = find_local_maxima_multivariable(0, 0, h, [delta_x_2, delta_y_2], 1)
+    print(x, y, h(x, y))
     # by maxima, gradient = [4*x-2*y,2*y-2*x-6] for the function y**2 - 2*x*y - 6*y + 2*x**2+12
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
